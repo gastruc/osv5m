a674a814facabedcaa43e0daea9ae56f0b01d6c7
ysig <y.siglidis@gmail.com>
2024-04-24 13:08:38 -0500

    update and reverse geo user-side

--- game/app.py
+++ game/app.py
@@ -3,9 +3,8 @@ import io
 import shutil 
 import os
 import uuid
-import matplotlib
 import time
-import pathlib
+import numpy as np
 
 from PIL import Image
 from math import radians, sin, cos, sqrt, asin, exp
@@ -28,7 +27,7 @@ from folium import Map, Element, LatLngPopup
 from matplotlib.offsetbox import AnchoredText
 
 
-IMAGE_FOLDER = './select'
+IMAGE_FOLDER = './images'
 CSV_FILE = './select.csv'
 RESULTS_DIR = './results'
 RULES = """<h1>OSV-5M (plonk)</h1>
@@ -67,6 +66,7 @@ h3 {
 """
 
 space_js = """
+<script src="https://cdn.jsdelivr.net/npm/@rapideditor/country-coder@5.2/dist/country-coder.iife.min.js"></script>
 <script>
 function shortcuts(e) {
     var event = document.all ? window.event : e;
@@ -80,9 +80,24 @@ function shortcuts(e) {
         }
     }
 }
+
+function shortcuts_exit(e) {
+    var event = document.all ? window.event : e;
+    switch (e.target.tagName.toLowerCase()) {
+        case "input":
+        case "textarea":
+        break;
+        default:
+        if (e.key.toLowerCase() == "e" && e.shiftKey) {
+            document.getElementById("exit_btn").click();
+        }
+    }
+}
 document.addEventListener('keypress', shortcuts, false);
+document.addEventListener('keypress', shortcuts_exit, false);
 </script>
 """
+
 
 def inject_javascript(folium_map):
     js = """
@@ -96,18 +111,14 @@ def inject_javascript(folium_map):
 
 def make_map_(name="map_name", id="1"):
     map = Map(location=[39, 23], zoom_start=1)
-    map._name, map._id = "map_name", "1"
+    map._name, map._id = name, id
 
     LatLngPopup().add_to(map)
     inject_javascript(map)
     return map
 
 def make_map(name="map_name", id="1"):
-    map = Map(location=[39, 23], zoom_start=1)
-    map._name, map._id = "map_name", "1"
-
-    LatLngPopup().add_to(map)
-    inject_javascript(map)
+    map = make_map_(name, id)
     fol = Folium(value=map, height=400, visible=False, elem_id='map-fol')
     return fol
 
@@ -119,7 +130,9 @@ def map_js():
         if (!latlng) { return; }
         textBox = `${latlng.lat},${latlng.lng}`;
         document.getElementById('coords-tbox').getElementsByTagName('textarea')[0].value = textBox;
-        return [a, `${latlng.lat},${latlng.lng}`];
+        var a = countryCoder.iso1A2Code([latlng.lat, latlng.lng]);
+        if (!a) { a = 'nan'; }
+        return [a, `${latlng.lat},${latlng.lng},${a}`];
     }
     """
 
@@ -140,10 +153,67 @@ def haversine(lat1, lon1, lat2, lon2):
 def geoscore(d):
     return 5000 * exp(-d / 1492.7)
 
+def compute_scores(csv_file):
+    df = pd.read_csv(csv_file)
+    if True: #'accuracy_country' not in df.columns:
+        print('Computing scores... (this may take a while)')
+        geocoders = rg.search([(row.true_lat, row.true_lon) for row in df.itertuples(name='Pandas')])
+        df['city'] = [geocoder['name'] for geocoder in geocoders]
+        df['area'] = [geocoder['admin2'] for geocoder in geocoders]
+        df['region'] = [geocoder['admin1'] for geocoder in geocoders]
+        df['country'] = [geocoder['cc'] for geocoder in geocoders]
+        print(list(zip((geocoders['cc']), [(row.true_lat, row.true_lon) for row in df.itertuples(name='Pandas')])))
+
+        df['city_val'] = df['city'].apply(lambda x: 0 if pd.isna(x) else 1)
+        df['area_val'] = df['area'].apply(lambda x: 0 if pd.isna(x) else 1)
+        df['region_val'] = df['region'].apply(lambda x: 0 if pd.isna(x) else 1)
+        df['country_val'] = df['country'].apply(lambda x: 0 if pd.isna(x) else 1)
+
+        df['distance'] = df.apply(lambda row: haversine(row['true_lat'], row['true_lon'], row['pred_lat'], row['pred_lon']), axis=1)
+        df['score'] = df.apply(lambda row: geoscore(row['distance']), axis=1)
+        df['distance_base'] = df.apply(lambda row: haversine(row['true_lat'], row['true_lon'], row['pred_lat_base'], row['pred_lon_base']), axis=1)
+        df['score_base'] = df.apply(lambda row: geoscore(row['distance_base']), axis=1)
+
+        print('Computing geocoding accuracy (base)...')
+        geocoders_base = rg.search([(row.pred_lat_base, row.pred_lon_base) for row in df.itertuples(name='Pandas')])
+        df['pred_city_base'] = [geocoder['name'] for geocoder in geocoders_base]
+        df['pred_area_base'] = [geocoder['admin2'] for geocoder in geocoders_base]
+        df['pred_region_base'] = [geocoder['admin1'] for geocoder in geocoders_base]
+        df['pred_country_base'] = [geocoder['cc'] for geocoder in geocoders_base]
+    
+        df['city_hit_base'] = [df['city'].iloc[i] != 'nan' and df['pred_city_base'].iloc[i] == df['city'].iloc[i] for i in range(len(df))]
+        df['area_hit_base'] = [df['area'].iloc[i] != 'nan' and df['pred_area_base'].iloc[i] == df['area'].iloc[i] for i in range(len(df))]
+        df['region_hit_base'] = [df['region'].iloc[i] != 'nan' and df['pred_region_base'].iloc[i] == df['region'].iloc[i] for i in range(len(df))]
+        df['country_hit_base'] = [df['country'].iloc[i] != 'nan' and df['pred_country_base'].iloc[i] == df['country'].iloc[i] for i in range(len(df))]
+
+        df['accuracy_city_base'] = [(0 if df['city_val'].iloc[:i].sum() == 0 else df['city_hit_base'].iloc[:i].sum()/df['city_val'].iloc[:i].sum()) for i in range(len(df))]
+        df['accuracy_area_base'] = [(0 if df['area_val'].iloc[:i].sum() == 0 else df['area_hit_base'].iloc[:i].sum()/df['area_val'].iloc[:i].sum()) for i in range(len(df))]
+        df['accuracy_region_base'] = [(0 if df['region_val'].iloc[:i].sum() == 0 else df['region_hit_base'].iloc[:i].sum()/df['region_val'].iloc[:i].sum()) for i in range(len(df))]
+        df['accuracy_country_base'] = [(0 if df['country_val'].iloc[:i].sum() == 0 else df['country_hit_base'].iloc[:i].sum()/df['country_val'].iloc[:i].sum()) for i in range(len(df))]
+
+        print('Computing geocoding accuracy (best)...')
+        geocoders = rg.search([(row.pred_lat, row.pred_lon) for row in df.itertuples()])
+        df['pred_city'] = [geocoder['name'] for geocoder in geocoders]
+        df['pred_area'] = [geocoder['admin2'] for geocoder in geocoders]
+        df['pred_region'] = [geocoder['admin1'] for geocoder in geocoders]
+        df['pred_country'] = [geocoder['cc'] for geocoder in geocoders]
+    
+        df['city_hit'] = [df['city'].iloc[i] != 'nan' and df['pred_city'].iloc[i] == df['city'].iloc[i] for i in range(len(df))]
+        df['area_hit'] = [df['area'].iloc[i] != 'nan' and df['pred_area'].iloc[i] == df['area'].iloc[i] for i in range(len(df))]
+        df['region_hit'] = [df['region'].iloc[i] != 'nan' and df['pred_region'].iloc[i] == df['region'].iloc[i] for i in range(len(df))]
+        df['country_hit'] = [df['country'].iloc[i] != 'nan' and df['pred_country'].iloc[i] == df['country'].iloc[i] for i in range(len(df))]
+
+        df['accuracy_city'] = [(0 if df['city_val'].iloc[:i].sum() == 0 else df['city_hit_base'].iloc[:i].sum()/df['city_val'].iloc[:i].sum()) for i in range(len(df))]
+        df['accuracy_area'] = [(0 if df['area_val'].iloc[:i].sum() == 0 else df['area_hit_base'].iloc[:i].sum()/df['area_val'].iloc[:i].sum()) for i in range(len(df))]
+        df['accuracy_region'] = [(0 if df['region_val'].iloc[:i].sum() == 0 else df['region_hit_base'].iloc[:i].sum()/df['region_val'].iloc[:i].sum()) for i in range(len(df))]
+        df['accuracy_country'] = [(0 if df['country_val'].iloc[:i].sum() == 0 else df['country_hit_base'].iloc[:i].sum()/df['country_val'].iloc[:i].sum()) for i in range(len(df))]
+        df.to_csv(csv_file, index=False)
+
 
 class Engine(object):
     def __init__(self, image_folder, csv_file, cache_path):
         self.image_folder = image_folder
+        self.csv_file = csv_file
         self.load_images_and_coordinates(csv_file)
         self.cache_path = cache_path
           
@@ -161,10 +231,13 @@ class Engine(object):
         df = pd.read_csv(csv_file)
 
         # Get the image filenames and their coordinates
-        self.images = df['id'].tolist()[:]
-        self.coordinates = df[['longitude', 'latitude']].values.tolist()[:]
-        self.admins = df[['city', 'sub-region', 'region', 'country']].values.tolist()[:]
-        self.preds = df[['pred_longitude', 'pred_latitude']].values.tolist()[:]
+        self.images = [os.path.join(self.image_folder, f"{img_path}.jpg") for img_path in df['id'].tolist()[:]]
+        self.coordinates = df[['true_lon', 'true_lat']].values.tolist()[:]
+
+        # compute the admins
+        self.df = df
+        self.admins = self.df[['city', 'area', 'region', 'country']].values.tolist()[:]
+        self.preds = self.df[['pred_lon', 'pred_lat']].values.tolist()[:]
 
     def isfinal(self):
         return self.index == len(self.images)-1
@@ -175,7 +248,7 @@ class Engine(object):
             self.finish()
 
         self.set_clock()
-        return os.path.join(self.image_folder, f"{self.images[self.index]}.jpg"), '### ' + str(self.index + 1) + '/' + str(len(self.images))
+        return self.images[self.index], '### ' + str(self.index + 1) + '/' + str(len(self.images))
 
     def get_figure(self):
         img_buf = io.BytesIO()
@@ -193,7 +266,7 @@ class Engine(object):
     def get_clock(self):
         return time.time() - self.time
 
-    def click(self, click_lon, click_lat):
+    def click(self, click_lon, click_lat, country):
         time_elapsed = self.get_clock()
         self.stats['times'].append(time_elapsed)
 
@@ -225,8 +298,15 @@ class Engine(object):
         score = geoscore(distance)
         self.stats['scores'].append(score)
         self.stats['distances'].append(distance)
-        
-        df = pd.DataFrame([self.get_model_average(who) for who in ['human', 'best', 'base']], columns=['who', 'GeoScore', 'Distance'])
+
+        # click = rg.search([(click_lat, click_lon)])[0]
+        # self.stats['city'].append(int(self.admins[self.index][0] != 'nan' and click['name'] == self.admins[self.index][0]))
+        # self.stats['area'].append(int(self.admins[self.index][1] != 'nan' and click['admin2'] == self.admins[self.index][1]))
+        # self.stats['region'].append(int(self.admins[self.index][2] != 'nan' and click['admin1'] == self.admins[self.index][2]))
+        self.stats['country'].append(int(self.admins[self.index][3] != 'nan' and country == self.admins[self.index][3]))
+
+        df = pd.DataFrame([self.get_model_average(who) for who in ['human', 'best', 'base']], columns=['who', 'GeoScore', 'Distance', 'Accuracy (country)']).round(2)
+        print(df)
         result_text = (f"### GeoScore: {score:.0f}, distance: {distance:.0f} km")
 
         self.cache(self.index+1, score, distance, (click_lat, click_lon), time_elapsed)
@@ -236,14 +316,32 @@ class Engine(object):
         # Go to the next image
         self.index += 1
         return self.load_image()
-        
-    def get_model_average(self, which):
+
+    def get_model_average(self, which, all=False):
+        aux, i = [], self.index+1
         if which == 'human':
             avg_score = sum(self.stats['scores']) / len(self.stats['scores']) if self.stats['scores'] else 0
             avg_distance = sum(self.stats['distances']) / len(self.stats['distances']) if self.stats['distances'] else 0
-            return [which, avg_score, avg_distance]
-        else:
-            return [which, 0, 0]
+            avg_country_accuracy = (0 if self.df['country_val'].iloc[:i].sum() == 0 else sum(self.stats['country'])/self.df['country_val'].iloc[:i].sum())
+            if all:
+                avg_city_accuracy = (0 if self.df['city_val'].iloc[:i].sum() == 0 else sum(self.stats['city'])/self.df['city_val'].iloc[:i].sum())
+                avg_area_accuracy = (0 if self.df['area_val'].iloc[:i].sum() == 0 else sum(self.stats['area'])/self.df['area_val'].iloc[:i].sum())
+                avg_region_accuracy = (0 if self.df['region_val'].iloc[:i].sum() == 0 else sum(self.stats['region'])/self.df['region_val'].iloc[:i].sum())
+                aux = [avg_city_accuracy, avg_area_accuracy, avg_region_accuracy]
+        elif which == 'base':
+            avg_score = np.mean(self.df[['score_base']].iloc[:i])
+            avg_distance = np.mean(self.df[['distance_base']].iloc[:i])
+            avg_country_accuracy = self.df['accuracy_country_base'].iloc[i]
+            if all:
+                aux = [self.df[['accuracy_city_base']].iloc[i], self.df[['accuracy_area_base']].iloc[i], self.df[['accuracy_region_base']].iloc[i]]
+        elif which == 'best':
+            avg_score = np.mean(self.df[['score']].iloc[:i])
+            avg_distance = np.mean(self.df[['distance']].iloc[:i])
+            avg_country_accuracy = self.df['accuracy_country'].iloc[i]
+            print('avg_country_accuracy', avg_country_accuracy)
+            if all:
+                aux = [self.df[['accuracy_city_base']].iloc[i], self.df[['accuracy_area_base']].iloc[i], self.df[['accuracy_region_base']].iloc[i]]
+        return [which, avg_score, avg_distance, avg_country_accuracy] + aux
 
     def update_average_display(self):
         # Calculate the average values
@@ -254,40 +352,14 @@ class Engine(object):
         return f"GeoScore: {avg_score:.0f}, Distance: {avg_distance:.0f} km"
     
     def finish(self):
-        clicks = rg.search(self.stats['clicked_locations'])
-        clicked_admins = [[click['name'], click['admin2'], click['admin1'], click['cc']] for click in clicks]
+        click = rg.search(self.stats['clicked_locations'])
+        self.stats['city'] = [(int(self.admins[self.index][0] != 'nan' and click['name'] == self.admins[self.index][0]))]
+        self.stats['area'] = [(int(self.admins[self.index][1] != 'nan' and click['admin2'] == self.admins[self.index][1]))]
+        self.stats['region'] = [(int(self.admins[self.index][2] != 'nan' and click['admin1'] == self.admins[self.index][2]))]
         
-        correct = [0,0,0,0]
-        valid = [0,0,0,0]
-        
-        for clicked_admin, true_admin in zip(clicked_admins, self.admins):
-            for i in range(4):
-                if true_admin[i]!= 'nan':
-                    valid[i] += 1
-                if true_admin[i] == clicked_admin[i]:
-                    correct[i] += 1
-                    
-        avg_city_accuracy = correct[0] / valid[0]
-        avg_area_accuracy = correct[1] / valid[1]
-        avg_region_accuracy = correct[2] / valid[2]
-        avg_country_accuracy = correct[3] / valid[3]
-        
-        avg_score = sum(self.stats['scores']) / len(self.stats['scores']) if self.stats['scores'] else 0
-        avg_distance = sum(self.stats['distances']) / len(self.stats['distances']) if self.stats['distances'] else 0
-
-        final_results = (
-            f"Average GeoScore: {avg_score:.0f}  \n" + 
-            f"Average distance: {avg_distance:.0f} km  \n" + 
-            f"Country Acc: {100*avg_country_accuracy:.1f}  \n" + 
-            f"Region Acc: {100*avg_region_accuracy:.1f}  \n" + 
-            f"Area Acc: {100*avg_area_accuracy:.1f}  \n" + 
-            f"City Acc: {100*avg_city_accuracy:.1f}"
-        )
-
-        self.cache_final(final_results)
-
-        # Update the text box
-        return f"# Your stats 🌍\n" + final_results + f"  \n# Thanks for playing ❤️"
+        df = pd.DataFrame([self.get_model_average(who, True) for who in ['human', 'best', 'base']], columns=['who', 'GeoScore', 'Distance'])
+        self.cache_final(df)
+        return df
         
     # Function to save the game state
     def cache(self, index, score, distance, location, time_elapsed):
@@ -332,28 +404,32 @@ if __name__ == "__main__":
         csv_str = os.environ['csv']
         with open(CSV_FILE, 'w') as f:
             f.write(csv_str)
-
+    
+    compute_scores(CSV_FILE)
     import gradio as gr
     def click(state, coords):
         if coords == '-1' or state['clicked']:
             return gr.update(), gr.update(), gr.update(), gr.update()
-        lat, lon = map(float, coords.split(','))
+        lat, lon, country = coords.split(',')
         state['clicked'] = True
-        image, text, df = state['engine'].click(lon, lat)
+        image, text, df = state['engine'].click(float(lon), float(lat), country)
         df = df.sort_values(by='GeoScore', ascending=False)
         return gr.update(visible=False), gr.update(value=image, visible=True), gr.update(value=text), gr.update(value=df, visible=True)
+
+    def exit_(state):
+        df = state['engine'].finish()
+        return gr.update(visible=False), gr.update(visible=False), gr.update(visible=False), gr.update(visible=False), gr.update(value=text), gr.update(visible=False), gr.update(value=df, visible=True), gr.update(value="-1", visible=False), gr.update(text=f"<h1> Your stats on OSV-5M🌍 </h1>", visible=True), gr.update(text='<h3>Thanks for playing ❤️</h3>', visible=True)
 
     def next_(state):
         if state['clicked']:
             if state['engine'].isfinal():
-                text = state['engine'].finish()
-                return gr.update(visible=False), gr.update(visible=False), gr.update(visible=False), gr.update(visible=False), gr.update(value=text), gr.update(visible=False), gr.update(visible=False), gr.update(value="-1", visible=False)
+                return exit_(state)
             else:
                 image, text = state['engine'].next_image()
                 state['clicked'] = False
-                return gr.update(value=make_map_(), visible=True), gr.update(visible=False), gr.update(value=image), gr.update(value=text), gr.update(), gr.update(), gr.update(visible=False), gr.update(value="-1")
+                return gr.update(value=make_map_(), visible=True), gr.update(visible=False), gr.update(value=image), gr.update(value=text), gr.update(), gr.update(), gr.update(visible=False), gr.update(value="-1"), gr.update(), gr.update()
         else:
-            return gr.update(), gr.update(), gr.update(), gr.update(), gr.update(), gr.update(), gr.update(), gr.update()
+            return gr.update(), gr.update(), gr.update(), gr.update(), gr.update(), gr.update(), gr.update(), gr.update(), gr.update()
 
     def start(state):
         # create a unique random temporary name under CACHE_DIR
@@ -382,12 +458,12 @@ if __name__ == "__main__":
             gr.update(visible=True),
         )
 
- # head=space_js
-    with gr.Blocks(css=css) as demo:
+ # 
+    with gr.Blocks(css=css, head=space_js) as demo:
         state = gr.State({})
         rules = gr.Markdown(RULES, visible=True)
 
-
+        exit_button = gr.Button("Exit", visible=False, elem_id='exit_btn')
         start_button = gr.Button("Start", visible=True)
         with gr.Row():
             map_ = make_map()
@@ -403,6 +479,7 @@ if __name__ == "__main__":
             ####
             next_button = gr.Button("Next", visible=False, elem_id='next')
         perf = gr.Dataframe(value=None, visible=False)
+        text_end = gr.Markdown("", visible=False)
     
         # map related
         coords = gr.Textbox(value="-1", label="Latitude, Longitude", visible=False, elem_id='coords-tbox')
@@ -410,6 +487,7 @@ if __name__ == "__main__":
 
         start_button.click(start, inputs=[state], outputs=[map_, results, image_, text_count, text, next_button, rules, state, start_button, coords, select_button])
         select_button.click(click, inputs=[state, coords], outputs=[map_, results, text, perf], js=map_js())
-        next_button.click(next_, inputs=[state], outputs=[map_, results, image_, text_count, text, next_button, perf, coords])
+        next_button.click(next_, inputs=[state], outputs=[map_, results, image_, text_count, text, next_button, perf, coords, rules, text_end])
+        exit_button.click(exit_, inputs=[state], outputs=[map_, results, image_, text_count, text, next_button, perf, coords, rules, text_end])
 
     demo.launch(allowed_paths=["custom.ttf"], debug=True)
